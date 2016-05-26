2b6ebbbe1fb4c3373047400b83f7c81e08041164
Hopptramp <rob-hopps@hotmail.co.uk>
2016-05-26 20:36:54 +0100

    More Level

---
+++ Assets/Scripts/BlockBehaviour.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BlockType
+{
+    Default,
+    Permanent,
+    Fragile,
+    HoldsData,
+    Pillar
+}
+
+public class BlockBehaviour : MonoBehaviour
+{
+    public BlockType blocktype;
+    float blockLifetime = 0;
+    bool dropBlock = false;
+
+    public void Init(int i, int defaultChance, int permanentChance, int fragileChance, int holdsDataChance, int pillarChance)
+    {
+        if (i < defaultChance)
+            blocktype = BlockType.Default;
+        else if (i < permanentChance)
+            blocktype = BlockType.Permanent;
+        else if (i < fragileChance)
+            blocktype = BlockType.Fragile;
+        else if (i < holdsDataChance)
+            blocktype = BlockType.HoldsData;
+        else if (i < pillarChance)
+            blocktype = BlockType.Pillar;
+
+        MeshRenderer mRenderer = GetComponent<MeshRenderer>();
+
+        switch(blocktype)
+        {
+            case BlockType.Default:
+                mRenderer.material = LevelGenerator.instance.blockMaterials[0];
+                blockLifetime = 3.0f;
+                break;
+            case BlockType.Permanent:
+                mRenderer.material = LevelGenerator.instance.blockMaterials[1];
+                break;
+            case BlockType.Fragile:
+                mRenderer.material = LevelGenerator.instance.blockMaterials[2];
+                break;
+            case BlockType.HoldsData:
+                mRenderer.material = LevelGenerator.instance.blockMaterials[3];
+                blockLifetime = 3.0f;
+                break;
+            case BlockType.Pillar:
+                mRenderer.material = LevelGenerator.instance.blockMaterials[4];
+                transform.localScale += new Vector3(0, 10, 0);
+                transform.localPosition -= new Vector3(0, 5, 0);
+                break;
+
+        }
+
+
+        print(blocktype);
+    }
+
+
+    public void HitBlock()
+    {
+        switch (blocktype)
+        {
+            case BlockType.Default:
+                Invoke("SetDropBlock", blockLifetime);
+                break;
+            case BlockType.Permanent:
+                // do nothing
+                break;
+            case BlockType.Fragile:
+                SetDropBlock();
+                break;
+            case BlockType.HoldsData:
+                Invoke("SetDropBlock", blockLifetime);
+                break;
+
+        }
+    }
+
+    void RemoveBlock()
+    {
+        LevelGenerator.instance.RemoveBlock(gameObject);
+        DestroyImmediate(gameObject);
+    }
+
+    void SetDropBlock()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+
+
+}
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -10,12 +10,14 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] GameObject dataPrefab = null;
     public float levelSize = 5;
     public float blockGap = 3;
+    [SerializeField] int dataHeightDistance = 5;
 
     [Tooltip("Define chances of each block appearing")] 
     [SerializeField] int defaultChance = 5;
     [SerializeField] int permanentChance = 3;
     [SerializeField] int fragileChance = 2;
     [SerializeField] int holdsDataChance = 5;
+    [SerializeField] int pillarChance = 4;
 
     [Tooltip("Ensure the materials are in the same order as the chances")]
     public Material[] blockMaterials;
@@ -32,12 +34,23 @@ public class LevelGenerator : MonoBehaviour
         permanentChance = defaultChance + permanentChance;
         fragileChance = permanentChance + fragileChance;
         holdsDataChance = fragileChance + holdsDataChance;
+        pillarChance = holdsDataChance + pillarChance;
 
         blockParent = new GameObject("BlockParent");
         blockParent.transform.parent = gameObject.transform;
 
         CreateLevel();
 
+    }
+
+    public void RemoveBlock(GameObject block)
+    {
+        blocks.Remove(block);
+    }
+
+    public void RemoveData(GameObject data)
+    {
+        datas.Remove(data);
     }
 
     void CreateLevel()
@@ -49,14 +62,14 @@ public class LevelGenerator : MonoBehaviour
                 GameObject block = Instantiate(blockPrefab, new Vector3(i * blockGap, gameObject.transform.position.y, j * blockGap), Quaternion.identity) as GameObject;
                 BlockBehaviour bBehave = block.AddComponent<BlockBehaviour>();
 
-                int blockType = Random.Range(0, holdsDataChance);
-                bBehave.Init(blockType, defaultChance, permanentChance, fragileChance, holdsDataChance);
+                int blockType = Random.Range(0, pillarChance);
+                bBehave.Init(blockType, defaultChance, permanentChance, fragileChance, holdsDataChance, pillarChance);
 
                 if(bBehave.blocktype == BlockType.HoldsData)
                 {
                     GameObject data = Instantiate(dataPrefab) as GameObject;
                     data.transform.parent = block.transform;
-                    data.transform.position = block.transform.position + new Vector3(0, -2, 0);
+                    data.transform.position = block.transform.position + new Vector3(0, -dataHeightDistance, 0);
 
                     datas.Add(data);
                 }
