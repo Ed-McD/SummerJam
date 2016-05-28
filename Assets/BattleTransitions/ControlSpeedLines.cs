8cd262303cd386047f09fe42eec4a7af448b0071
Ed-McD <edard_666@hotmail.co.uk>
2016-05-28 15:27:01 +0100

    A Shader borrowed from youtube
    used Makin stuff look goods transition shader and rewrote it a little.
    Now we have speed lines appear on camera when you are moving fast, it
    looks alright

---
+++ Assets/BattleTransitions/ControlSpeedLines.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlSpeedLines : MonoBehaviour {
+
+    public AnimationCurve curve;
+    public Material TransitionMaterial;
+    public float lowerBound = 0;
+    public float upperBound;
+    
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody>();        
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float range = upperBound - lowerBound;
+        float val = (rb.velocity.magnitude-lowerBound)/range;
+        val = Mathf.Clamp(val, 0, 1);
+        TransitionMaterial.SetFloat("_Cutoff", curve.Evaluate(val));
+
+    }
+}
