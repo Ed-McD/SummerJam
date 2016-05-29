7a16386dd52c43e449f032e611d660c9dc9ac7f3
Ed-McD <edard_666@hotmail.co.uk>
2016-05-29 13:50:11 +0100

    Made an an enemy that moves around

---
+++ Assets/AIController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIController : MonoBehaviour {
+
+    public List<Vector3> Waypoints = new List<Vector3>();
+    int progression = 0;
+    [SerializeField] private float leeway = 0;
+    [SerializeField] private float speed =1;
+    [SerializeField] private float rotSpeed = 10;
+
+
+    [SerializeField]
+    private float coolDownLength;
+    private float cdTimer = 0.0f;
+    private bool cdActive = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CoolDown();
+        Move();
+    }
+    void CoolDown()
+    {
+        if (cdActive)
+        {
+            cdTimer += Time.deltaTime;
+            if (cdTimer>= coolDownLength)
+            {
+                cdActive = false;
+                cdTimer = 0.0f;
+            }
+        }
+    }
+    void Move()
+    {
+        if (Waypoints.Count > 0)
+        {
+            if (Vector3.Distance(transform.position, Waypoints[progression]) <= leeway)
+            {
+                progression = progression >= (Waypoints.Count - 1) ? progression = 0 : progression+1;
+            }
+            if (transform.rotation == Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Waypoints[progression] - transform.position), rotSpeed * Time.fixedDeltaTime))
+            {
+                transform.position = Vector3.MoveTowards(transform.position, Waypoints[progression], speed * Time.fixedDeltaTime);
+            }
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Waypoints[progression]-transform.position), rotSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
+
