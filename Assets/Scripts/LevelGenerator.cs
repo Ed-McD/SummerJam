57649af0e2693e46a77a40301eb3f3a17a3870df
Hopptramp <rob-hopps@hotmail.co.uk>
2016-05-31 15:46:54 +0100

    stuff

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -8,6 +8,8 @@ public class LevelGenerator : MonoBehaviour
 
     [SerializeField] GameObject blockPrefab = null;
     [SerializeField] GameObject dataPrefab = null;
+    [SerializeField] GameObject outerSphere = null;
+
     public int levelSize = 5;
     public float blockGap = 3;
     [SerializeField] float blockScale = 1;
@@ -22,6 +24,12 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] int pillarChance = 4;
     [SerializeField] int pillarScale = 20;
 
+    [SerializeField] bool debugScale = true;
+    [Tooltip("0, 1, 2 increasing scales of level")]
+    public int defaulScale = 0;
+
+    int centralPoint = 0;
+    Vector3 sphereScale;
     private int blocksDropped = 0;
 
     [Tooltip("Ensure the materials are in the same order as the chances")]
@@ -44,10 +52,33 @@ public class LevelGenerator : MonoBehaviour
         blockParent = new GameObject("BlockParent");
         blockParent.transform.parent = gameObject.transform;
 
+        Init();
+    }
+
+    public void Init()
+    {
+        SetScales(defaulScale);
+        centralPoint = levelSize / 2;
         CreateLevel();
+    }
 
-
-
+    public void SetScales(int scale)
+    {
+        switch(scale)
+        {
+            case 0:
+                levelSize = 10;
+                sphereScale = new Vector3(300, 300, 300);
+                break;
+            case 1:
+                levelSize = 20;
+                sphereScale = new Vector3(400, 400, 400);
+                break;
+            case 2:
+                levelSize = 30;
+                sphereScale = new Vector3(500, 500, 500);
+                break;
+        }
     }
 
     public void RemoveBlock(GameObject block)
@@ -105,6 +136,12 @@ public class LevelGenerator : MonoBehaviour
 
                 block.transform.parent = blockParent.transform;
                 blocks.Add(block);
+
+                if(i == centralPoint && j == centralPoint)
+                {
+                    GameObject sphere = Instantiate(outerSphere, new Vector3(i * blockGap, transform.position.y, j * blockGap), Quaternion.identity) as GameObject;
+                    sphere.transform.localScale = sphereScale;
+                }
             }
         }
         AIManager.instance.Populate(levelSize, blockGap, pillarScale, blockScale);
