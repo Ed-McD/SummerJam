eff597d049f14d596c273153c3df30adb50b6bc6
Ed-McD <edard_666@hotmail.co.uk>
2016-05-31 12:19:20 +0100

    Menu + a sphere

---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour {        
+
+	public void LoadLevel(int _level)
+    {
+        if (_level <0 || _level >= SceneManager.sceneCount)
+        {
+            SceneManager.LoadScene(_level);
+        }
+    }
+    public void LoadLevel(string _level)
+    {
+        if (SceneManager.GetSceneByName(_level) != null)
+        {
+            SceneManager.LoadScene(_level);
+        }
+    }
+
+
+}
