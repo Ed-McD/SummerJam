7790ddb06ab967acb85e68aafcaa4a7b2791ea5b
Ed-McD <edard_666@hotmail.co.uk>
2016-05-31 14:20:53 +0100

    slowing time son

---
+++ Assets/Scripts/SlowTime.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowTime : MonoBehaviour {
+
+    [SerializeField] private float slowMo = 0.75f;
+    [SerializeField] private float maxTime = 5.0f;
+    public  bool canSlow;
+    public float meterLevel;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        meterLevel = maxTime;
+        canSlow = true;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("left shift") && meterLevel > 0 && canSlow)
+        {
+            Time.timeScale = slowMo;
+            meterLevel -= Time.fixedDeltaTime / slowMo;
+        }
+        else
+        {            
+            Time.timeScale = 1;
+            meterLevel += Time.fixedDeltaTime;
+            if (meterLevel > maxTime)
+                canSlow = true;
+        }
+        if(meterLevel <= 0)
+        {            
+            canSlow = false;
+        }
+        meterLevel = Mathf.Clamp(meterLevel, 0, maxTime);
+
+        //Update slider here
+
+        
+    }
+    public void ResetMeter()
+    {
+        meterLevel = maxTime;
+        //Update slider here
+    }
+}
